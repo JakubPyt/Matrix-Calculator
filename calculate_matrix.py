6ffa07a0c189b231823d2db96da5039c03969bb1
JakubPyt <pyt.jakub@gmail.com>
2021-05-06 16:05:19 +0200

    calculate_matrix/calculation section

--- calculate_matrix.py
+++ calculate_matrix.py
@@ -2,3 +2,30 @@ import numpy as np
 
 
 def calculate_matrix(numbers):
+    # ======================
+    # Calculation section
+    # ======================
+    # Reshape list to matrix 3x3
+    matrix = np.array(numbers).reshape(3, 3)
+    # Here here we will get the results
+    results = {}
+    # Here we have functions which we have to do
+    actions = {
+        "Mean": np.mean,
+        "Variance": np.var,
+        "Standard deviation": np.std,
+        "Max": np.max,
+        "Min": np.min,
+        "Sum": np.sum
+    }
+    # For each function from actions
+    for k, v in actions.items():
+        # We calculate function for entire matrix
+        results[k] = {"The entire matrix": v(matrix)}
+        # And then, we calculate function for each row and column
+        for index in range(len(matrix)):
+            # And save results to results dictionary
+            results[k].update({
+                f"Row {index + 1}": v(matrix[index, :]),
+                f"Column {index + 1}": v(matrix[:, index])
+            })
